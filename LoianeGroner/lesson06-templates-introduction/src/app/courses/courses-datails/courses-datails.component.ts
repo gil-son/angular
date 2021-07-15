021fbff3ef965193f8ff2ae03ab78b77d40f1637
gil-son <gilszon@gmail.com>
2021-07-15 20:28:37 -0300

    Introduction to Templates

---
+++ LoianeGroner/lesson06-templates-introduction/src/app/courses/courses-datails/courses-datails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-datails',
+  templateUrl: './courses-datails.component.html',
+  styleUrls: ['./courses-datails.component.css']
+})
+export class CoursesDatailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
