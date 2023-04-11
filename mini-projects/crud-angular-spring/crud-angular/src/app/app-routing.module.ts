4e8088faf2eaf8c0133c6bbd3cfcc2f38b8ef16f
gil-son <gilszon@gmail.com>
2023-04-11 17:56:47 -0300

    Created Courses Module

--- mini-projects/crud-angular-spring/crud-angular/src/app/app-routing.module.ts
+++ mini-projects/crud-angular-spring/crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', pathMatch: 'full', redirectTo: 'courses' }, // Any path will direct to courses,
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)   // Lazy lode mode from child
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
